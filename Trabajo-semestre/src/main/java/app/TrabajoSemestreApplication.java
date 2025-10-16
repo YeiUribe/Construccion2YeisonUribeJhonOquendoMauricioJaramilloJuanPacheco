533b6f72dae4634fbe7fd5548ab061d54b860ce4
YeiUribe <ceferibe@gmail.com>
2025-10-15 23:03:25 -0500

    initial commit

---
+++ Trabajo-semestre/src/main/java/app/TrabajoSemestreApplication.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoSemestreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoSemestreApplication.class, args);
+	}
+
+}
